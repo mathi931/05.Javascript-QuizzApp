67fb90339834b75f6f15de69a299cabf40d31261
unknown <mesi.mathi@gmail.com>
2021-02-13 22:56:10 +0100

    added fully functional logic & new page for result

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//some data
 const quizData = [
 	{
 		id: 0,
@@ -34,8 +35,8 @@ const quizData = [
 		question:
 			'In 2013 which two airlines merged to become the world’s largest airline?',
 		op1: 'Air France and KLM',
-		op2: 'American Airlines and US Airways',
-		op3: 'Lufthansa and Germanwings',
+		op2: 'Lufthansa and Germanwings',
+		op3: 'American Airlines and US Airways',
 		op4: 'British Airways and Iberia',
 		correct: 'American Airlines and US Airways',
 		asked: false,
@@ -51,38 +52,112 @@ const quizData = [
 		asked: false,
 	},
 ];
+
+//elements
 const QuestionText = document.getElementById('questionText');
 const Options = document.getElementsByClassName('options');
 const Submit = document.getElementById("button-submit");
-let questionIndex = [];
-let lastI;
 
-//
+// an array what helps to follow the index of the questions. The last value is the current questions ID
+const questionIndex = [];
+// a variable follows the option's index. if nothing is selected = undefined
+let optionI;
+// selected option element
+let currentOptionElement;
 
-//screen loaded
-window.addEventListener('DOMContentLoaded', function () {
-	questionIndex.push(3);
-	loadQuiz();
-	console.log();
+
+//Submit Button Click Event
+Submit.addEventListener("click", function(){
+	//0.chek if its submit or nextquestion function and still have question what is not asked already
+	if (Submit.innerHTML === "Submit") {
+		//1.check if there is a selected option
+		if (optionI !== undefined) {	
+
+			//2.check if it is a correct answer
+			if (GetAnswerResult() && questionIndex.length < 5) {
+				//correct answer -> set icon, and hinghlight
+				ShowIcons(true);
+				HighlightCorrect(true);
+			}
+			else{
+				//false answer -> highlight correct answer, set icons
+				ShowIcons(false);
+				HighlightCorrect(false);
+			}
+			//4.change button Submit to Next question
+			if (questionIndex.length < 5) {
+				SwitchButton (false);
+			}
+			//last question so switch to results
+			else{
+				alert("DONE");
+				window.location = 'result.html';
+			}
+			
+			
+			
+		}
+		else {
+			alert("You must select an option first!");
+		}
+
+	}
+	//
+	else if (Submit.innerHTML === "Next Question") {
+		//5.go to next question
+		questionIndex.push(GetRandomNumberNorRepeat(0,5));
+		loadQuiz();
+		//6.button back to Submit
+		SwitchButton(true);
+		ResetClasses();
+	}
+	
+	else{
+		alert("Something is wrong bro");
+	}
+		
 
 });
+//Screen Load Event
+window.addEventListener('DOMContentLoaded', function () {
+
+	if (window.location.href == "http://127.0.0.1:5500/index.html") {
+		questionIndex.push(GetRandomNumber(0,5));
+		loadQuiz();
+	}
+	else if (window.location.href == "http://127.0.0.1:5500/result.html") {
+		
+	}
+});
+//On Option Click Event
+function OptionIsSelected(option){
+	OptionClassChange(option);
+}
+function OptionClassChange(option){
+	//first select
+	if (optionI === undefined){
+		option.classList.add("option-selected");
+		optionI = option.id;	
+	}
+	//select other option
+	else if (!option.className.includes("option-selected") && option.id !== optionI && option.id !== undefined) {
+		Options[optionI].classList.remove("option-selected");
+		option.classList.add("option-selected");
+		optionI = option.id;
+	}
+	else if (option.className.includes("option-selected") && option.id === optionI && option.id !== undefined) {
+		option.classList.remove("option-selected");
+		optionI = undefined;
+	}
 
 
-//random number but nor repeat to the indexArray
-function getRandomNumberNorRepeat(min, max) {
-	var next = Math.floor(Math.random() * (max - min)) + min;
-	questionIndex.includes(next)
-		? getRandomNumberNorRepeat(min, max)
-		: questionIndex.push(next);
+	currentOptionElement =  option;
 }
 
-//Returns a random number between min (inclusive) and max (exclusive)
-function getRandomNumber(min, max) {
-	return Math.floor(Math.random() * (max - min)) + min;
-}
 
-//elements
+//elements & Set functions
 function loadQuiz() {
+
 	loadQuestion();
 	loadOptions();
 	//console.log(questionIndex[questionIndex.length - 1]);
@@ -101,32 +176,68 @@ function loadOptions() {
 	Options[3].children[0].innerHTML =
 		quizData[questionIndex[questionIndex.length - 1]].op4;
 }
-//selected options text
-function OptionIsSelected(option){
-    console.log(option);
-	OptionClassChange(option);
+function ShowIcons(correct){
+	if (correct) {
+		currentOptionElement.children[1].classList.add("fas", "fa-check");
+	}
+	else {
+		currentOptionElement.children[1].classList.add("fas", "fa-times");
+		Options[GetCurrentQuizCorrectAnswer()].children[1].classList.add("fas", "fa-check");
+	}
+}
+function HighlightCorrect(correct){
+	//answer is correct -> highligh Options[currentIndex], hide others
+	if (correct) {
+		for (let i = 0; i < Options.length; i++) {
+			Options[i].classList.add("option-wrong");
+		}
+		Options[optionI].classList.replace("option-wrong", "options-correct");
+	}
+	else{
+		for (let i = 0; i < Options.length; i++) {
+			Options[i].classList.add("option-wrong");
+		}
+		currentOptionElement.classList.remove("option-selected");
+		Options[GetCurrentQuizCorrectAnswer()].classList.add("option-selected");
+		Options[GetCurrentQuizCorrectAnswer()].classList.replace("option-wrong", "options-correct");
 
-    //return option.children[0].innerHTML;
+	}}
+function SwitchButton (toSubmit){
+	return toSubmit ? Submit.innerHTML = "Submit": Submit.innerHTML = "Next Question";
+}
+function ResetClasses() {
+	for (let i = 0; i < Options.length; i++) {
+		Options[i].classList.remove("option-selected", "option-wrong", "option-correct");
+		Options[i].children[1].classList.remove("fas", "fa-check", "fa-times");
+	}
 }
 
-function OptionClassChange(option){
-	//first select
-	if (lastI === undefined){
-		option.classList.add("option-selected");
-		lastI = option.id;	
-	}
-	//select other option
-	else if (!option.className.includes("option-selected") && option.id !== lastI && option.id !== undefined) {
-		Options[lastI].classList.remove("option-selected");
-		option.classList.add("option-selected");
-		lastI = option.id;
-	}
-	else if (option.className.includes("option-selected") && option.id === lastI && option.id !== undefined) {
-		option.classList.remove("option-selected");
-		lastI = undefined;
-	}
 
-	//return option.classList;
+//Get functions
+function GetAnswerResult(){
+	return currentOptionElement.innerText === quizData[questionIndex[questionIndex.length-1]].correct ? true : false;
 }
-//console.log(Options[lastI]);
+function GetCurrentQuizCorrectAnswer(){
+	var correctAnswer = quizData[questionIndex[questionIndex.length-1]].correct;
 
+	//we need the correct answer's index from the option list
+
+	for (let i = 0; i < Options.length; i++) {
+		if(Options[i].children[0].innerHTML === correctAnswer)
+		{
+			return i;
+		}
+	}
+}
+//random number but nor repeat to the indexArray
+function GetRandomNumberNorRepeat(min, max) {
+	var next = Math.floor(Math.random() * (max - min)) + min;
+	if (questionIndex.includes(next)) {
+		next = GetRandomNumberNorRepeat(min, max);
+	}
+	return next;
+}
+//Returns a random number between min (inclusive) and max (exclusive)
+function GetRandomNumber(min, max) {
+return Math.floor(Math.random() * (max - min)) + min;
+}
